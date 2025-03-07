7998109fa608669f001cbb996fbe0d8fe3458f33
t7897 <tm07081997@gmail.com>
2025-03-07 11:08:31 +0100

    Risolto bug nell'IA del nemico
    C'è un evento che viene lanciato e non ho capito come gestirlo. Siccome non penso mi serva (per ora), ho creato un duplicato dell'animazione, ho eliminato l'evento e sto usando la copia all'interno dell'animator del nemico.
    
    Sarà necessario fare un polish di tutto il pacchetto per capire cosa viene usato, cosa non viene usato e creare degli animator personalizzati per il gioco.

---
+++ Ripeat/Assets/Scripts/EnemyAI/EnemyBehaviour.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using FreeFlowCombatSpace;
+using System.Linq;
+using Unity.VisualScripting;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    //Reference al component FreeflowCombat
+    [SerializeField] private FreeflowCombat freeflowCombat;
+
+    /*
+        //Implementazione a seguire
+    */
+
+    //Riferimento al Player
+    [SerializeField] private Transform playerTransform;
+    //Raggio di attacco
+    [SerializeField] private float attackRange = 2.5f;
+
+    //Espandere per futuri sviluppi
+    enum EnemyStatus {
+        //Status dell'IA del nemico
+        FOLLOWING_PLAYER, ATTACK, IDLE
+    }
+
+    [SerializeField] private EnemyStatus enemyStatus = EnemyStatus.IDLE;
+
+    [SerializeField] private float pauseAttack = 2.0f;
+    [SerializeField] private bool hasAttacked = false;
+
+    //Errore con la dicitura seguente:
+    /*
+    
+    'Enemy' AnimationEvent 'StopHit' on animation 'combo_01_1' has no receiver! Are you missing a component?
+    
+    */
+
+    //Risolto eliminando l'evento in un duplicato dell'animazione ed usando quello.
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //freeflowCombat = GetComponent<FreeflowCombat>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void StopHit()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(enemyStatus == EnemyStatus.FOLLOWING_PLAYER)
+        {
+            MoveTowardsPlayer();
+        }
+        else if(enemyStatus == EnemyStatus.ATTACK)
+        {
+            AttackPlayer();
+        }
+
+        UpdateAIStatus();
+        
+
+    }
+
+    private void UpdateAIStatus()
+    {
+        // enemyStatus = EnemyStatus.IDLE; 
+        // GetComponent<Animator>().SetInteger("AttackType", 0);
+        // GetComponent<Animator>().SetBool("Run", false);
+        
+
+        if(enemyStatus != EnemyStatus.FOLLOWING_PLAYER && Vector3.Distance(transform.position, playerTransform.position) > attackRange)
+        {
+            enemyStatus = EnemyStatus.FOLLOWING_PLAYER;
+            GetComponent<Animator>().SetInteger("AttackType", 0);
+        }
+        else if(enemyStatus != EnemyStatus.ATTACK && Vector3.Distance(transform.position, playerTransform.position) <= attackRange)
+        {
+            enemyStatus = EnemyStatus.ATTACK;
+        }
+
+    }
+
+    private void AttackPlayer()
+    {
+        
+        GetComponent<Animator>().SetBool("Run", false);
+        if(Vector3.Distance(transform.position, playerTransform.position) <= attackRange && !hasAttacked)
+        {
+            GetComponent<Animator>().SetInteger("AttackType", 1);
+            //freeflowCombat.Attack();
+            StartCoroutine(AttackWaitingTime());
+        }
+
+        
+    }
+
+    IEnumerator AttackWaitingTime()
+    {
+        hasAttacked = true;
+        GetComponent<Animator>().SetInteger("AttackType", 1);
+        yield return new WaitUntil( () => GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f );
+        GetComponent<Animator>().SetInteger("AttackType", 0);
+        yield return new WaitForSeconds(pauseAttack);
+        hasAttacked = false;
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        if(Vector3.Distance(transform.position, playerTransform.position) > attackRange)
+        {
+            // Vector3 movement = transform.position - playerTransform.position;
+
+            // transform.Translate(movement.normalized * 2.5f * Time.deltaTime);
+            
+            // freeflowCombat.xInput = movement.normalized.x;
+            // freeflowCombat.yInput = movement.normalized.z;
+            GetComponent<Animator>().SetBool("Run", true);
+        }
+        
+    }
+
+
+
+
+
+}
