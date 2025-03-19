978adb20a24fc55bb6e020bd2ef1f5af063380cd
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-03-19 21:13:56 +0100

    Primo Commit combat system nuovo

---
+++ Ripeat/Assets/Scripts/New Combat System/CombatSystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CombatSystem : MonoBehaviour
+{
+
+    //Enum x gli stati in cui si trova il personaggio
+    public enum CharacterState
+    {
+        IDLE, KICK, PUNCH, BLOCK, DEAD
+    };
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
