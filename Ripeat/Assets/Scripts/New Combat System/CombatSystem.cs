583b90ef62a2115d0805cc29a4e77dc107e6d3c9
t7897 <tm07081997@gmail.com>
2025-03-20 11:00:22 +0100

    Struttura script combat system

--- Ripeat/Assets/Scripts/New Combat System/CombatSystem.cs
+++ Ripeat/Assets/Scripts/New Combat System/CombatSystem.cs
@@ -6,8 +6,69 @@ public class CombatSystem : MonoBehaviour
     //Enum x gli stati in cui si trova il personaggio
     public enum CharacterState
     {
-        IDLE, KICK, PUNCH, BLOCK, DEAD
+        IDLE, MOVING, KICK, PUNCH, BLOCK, DEAD
     };
+
+    //Properties per gestire input e stato del personaggio (se non è morto!)
+    private Vector3 movementInput;
+
+    public Vector3 MovementInput
+    {
+        get { return movementInput; }
+        set
+        {
+            if(currentState != CharacterState.DEAD)
+            {
+                movementInput = value;
+            }
+        }
+    }
+
+    private CharacterState currentState = CharacterState.IDLE;
+
+    public CharacterState CurrentState
+    {
+        get { return currentState; }
+        set
+        {
+            if (currentState != CharacterState.DEAD) // Se sono morto, non posso cambiare stato
+            {
+                currentState = value;
+                UpdateAnimationState();
+            }
+        }
+    }
+
+    //Riferimento all'Animator del personaggio
+    private Animator animator;
+
+
+
+    //Aggiorna lo stato dell'Animator in base allo stato del personaggio (va chiamato in Update)
+    void UpdateAnimationState()
+    {
+        switch (currentState)
+        {
+            case CharacterState.IDLE:
+                
+                break;
+            case CharacterState.MOVING:
+                
+                break;
+            case CharacterState.PUNCH:
+                
+                break;
+            case CharacterState.KICK:
+                
+                break;
+            case CharacterState.BLOCK:
+                
+                break;
+            case CharacterState.DEAD:
+                
+                break;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +79,6 @@ public class CombatSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateAnimationState();
     }
 }
