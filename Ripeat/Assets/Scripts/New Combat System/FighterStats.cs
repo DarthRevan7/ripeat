a9cb693c69f3eae18f422ed6f39da8c454646662
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-03-27 12:14:18 +0100

    Collegate animazioni ad eventi di combattimento

--- Ripeat/Assets/Scripts/New Combat System/FighterStats.cs
+++ Ripeat/Assets/Scripts/New Combat System/FighterStats.cs
@@ -5,7 +5,24 @@ public class FighterStats : MonoBehaviour
 
     public int vita;
     public float movementSpeed = 5f;
-    
+
+    [SerializeField] private float colliderRadius = 5f;
+
+
+    public void Hit()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position,colliderRadius);
+
+        foreach (Collider collider in colliders)
+        {
+            if(collider.gameObject.name.Equals("MyEnemyNew"))
+            {
+                Debug.Log("Nemico Colpito!!");
+            }
+        }
+        GetComponent<CombatSystem>().CurrentState = CombatSystem.CharacterState.IDLE;
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
