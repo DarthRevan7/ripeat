6d9b499940e6448ba29713de2772f0cbd63bf2cc
andredelu98 <85584741+andredelu98@users.noreply.github.com>
2025-05-16 17:50:01 +0200

    Aggiunta fulmine e cambio colore luce scena iniziale

--- Ripeat/Assets/Scripts/Event System/EventHandler.cs
+++ Ripeat/Assets/Scripts/Event System/EventHandler.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DigitalRuby.LightningBolt;
 using FreeflowCombatSpace;
 using UnityEngine;
 using UnityEngine.SceneManagement;
