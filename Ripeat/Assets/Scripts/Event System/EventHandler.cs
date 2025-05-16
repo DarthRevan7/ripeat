678f02f0a12cd73cb5e001399cc3876725474952
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-05-16 17:51:20 +0200

    piccole imperfezioni

--- Ripeat/Assets/Scripts/Event System/EventHandler.cs
+++ Ripeat/Assets/Scripts/Event System/EventHandler.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DigitalRuby.LightningBolt;
-using FreeflowCombatSpace;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
