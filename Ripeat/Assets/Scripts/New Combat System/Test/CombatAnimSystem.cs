8fb291b8f8019e11522f23c23a714f263a40e722
t7897 <tm07081997@gmail.com>
2025-06-01 18:41:05 +0200

    Riprogrammazione del combat system con le fasi di animazione

---
+++ Ripeat/Assets/Scripts/New Combat System/Test/CombatAnimSystem.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+
+public class CombatAnimSystem : MonoBehaviour
+{
+    public enum CombatAnimState
+    {
+        IDLE, PUNCH, KICK, BLOCK, MOVING, DEAD
+    }
+
+    [SerializeField] private CombatAnimState combatState;
+
+    public CombatAnimState CurrentState
+    {
+        get { return combatState; }
+        set
+        {
+            combatState = value;
+        }
+    }
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private string punchAnimName, kickAnimName, blockAnimName, deadAnimName, idleAnimName, runAnimName;
+
+    /*
+    
+    Serve per realizzare la reattività del fight:
+    0 -> Animazione default (idle)
+    1 -> Pre-Execution (l'azione può essere modificata)
+    2 -> Execution
+    3 -> Post-Execution (calcolo danni e knockback)
+
+    Nel caso spostiamo calcolo danni in execution? Da capire.
+    
+    */
+    [SerializeField] private int animState = 0;
+
+    public void SetAnimState(int numState)
+    {
+        animState = numState;
+        AnimationTest();
+    }
+
+
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RequestStateChange(CombatAnimState.KICK);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RequestStateChange(CombatAnimState.PUNCH);
+        }
+    }
+
+    public void RequestStateChange(CombatAnimState state)
+    {
+        if (StateChangeCheck())
+        {
+            CurrentState = state;
+            ExecuteAnimationChange();
+        }
+    }
+    /*
+    La window of opportunity per tentare di cambiare stato è in pre-execution (oppure anticipation).
+    */
+    public bool StateChangeCheck()
+    {
+        return animState < 2;
+    }
+    public void ExecuteAnimationChange()
+    {
+        if (!StateChangeCheck())
+            return;
+        //Ferma l'animazione corrente (devo trovare il metodo adatto da chiamare)
+            switch (CurrentState)
+            {
+                case CombatAnimState.PUNCH:
+                    animator.Play(punchAnimName);
+                    break;
+                case CombatAnimState.KICK:
+                    animator.Play(kickAnimName);
+                    break;
+                case CombatAnimState.BLOCK:
+                    animator.Play(blockAnimName);
+                    break;
+                case CombatAnimState.MOVING:
+                    animator.SetBool("Run", true);
+                    break;
+                case CombatAnimState.DEAD:
+                    animator.SetTrigger("Die");
+                    break;
+                default:
+                    animator.SetBool("Run", false);
+                    break;
+            }
+    }
+    
+    #region Test
+    void AnimationTest()
+    {
+        Debug.Log(animState.ToString() + " State");
+    }
+    #endregion
+}
