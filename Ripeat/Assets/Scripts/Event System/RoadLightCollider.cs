1e2c0936dfc083db3987aea9e74f14027e41b3e7
t7897 <tm07081997@gmail.com>
2025-06-19 20:56:23 +0200

    Bugfixing

--- Ripeat/Assets/Scripts/Event System/RoadLightCollider.cs
+++ Ripeat/Assets/Scripts/Event System/RoadLightCollider.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoadLightCollider : MonoBehaviour
 {
     
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag.Equals("Player"))
+        if(other.gameObject.tag.Equals("Player") && SceneManager.GetActiveScene().name == "CombatScene")
         {
             EventHandler.Instance.streetlamp = gameObject;
         }
